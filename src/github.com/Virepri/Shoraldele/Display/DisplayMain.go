87a18b45735572e868df0d4f188b85a502d28620
Joshua Brockschmidt <joshypoo@SoraSpare>
2017-05-21 04:02:41 -0700

    It does stuff!!!!!!!

--- src/github.com/Virepri/Shoraldele/Display/DisplayMain.go
+++ src/github.com/Virepri/Shoraldele/Display/DisplayMain.go
@@ -9,24 +9,20 @@ import (
 
 func DisplayInit(z string) {
 	framebuffer := termo.NewFramebuffer(80, 20) 
-	var fill string
-	var State termo.CellState
 
-	fill = "abcdefgijklmnopqrstuvwxyz"
+	var State termo.CellState
 
 	State.Attrib = 0
 	State.FGColor = 30
 	State.BGColor = 80
 
-	buffer.Insert(1, fill)
-
 	termo.Init() //Init stuff
-	for i := 0; i < 10; i++ {
+	for {
 		
 		framebuffer.ASCIIRect(0, 0, 80, 20, true, false)//Here we draw an ascii rectangle of size 40x40
 
-		framebuffer.AttribText(1, 1, State, string(buffer.GetBufferContents(0, 20)))
-		buffer.Insert(1, string(i))
+		framebuffer.AttribText(1, 1, State, string(buffer.GetBufferContents(0, -1)))
+
 		framebuffer.Flush()
 
 		framebuffer.Clear()
