6d91ffce5031f378a7980f256c8d35c8e745999d
Adele Lynn <virepri2k@gmail.com>
2017-05-20 18:02:42 -0700

    Removed commands system. Check start.go for info.

--- src/github.com/Virepri/Shoraldele/GlobalVars/GlobalVars.go
+++ src/github.com/Virepri/Shoraldele/GlobalVars/GlobalVars.go
@@ -4,6 +4,5 @@ import "sync"
 
 var ConfigLocs map[string]string
 var SetupFuncs map[string]func(string) //runs on program startup
-var CmdFuncs map[string]func(string,string)
 var ModuleRoutines map[string]func()
 var WaitGroup sync.WaitGroup
--- src/github.com/Virepri/Shoraldele/start.go
+++ src/github.com/Virepri/Shoraldele/start.go
@@ -10,7 +10,7 @@ import(
 ADDING YOUR MODULE:
 You'll need 3 functions at a minimum, as well as a config location (can be empty)
 Setup: Recieves a string. Reads the config, sets up.
-Command: Recieves 2 strings. Executes the corresponding command (string 1) with the arguments (string 2)
+Commands: Capitalize anything you want others to use. Document them at the top of your file.
 Routine: Recieves nothing. Should _ALWAYS_ be running until you recieve the "stop" command.
 
 Basically, add a new line to each of the below maps with this setup:
@@ -22,7 +22,6 @@ and then, to the import statement, add the directory path to your module, ignori
 func main(){
 	GlobalVars.ConfigLocs = map[string]string{} //basically add your config location here.
 	GlobalVars.SetupFuncs = map[string]func(string) {} //basically add your setup functions here. the input is meant to be a config location.
-	GlobalVars.CmdFuncs = map[string]func(string,string) {} //add your command function here
 	GlobalVars.ModuleRoutines = map[string]func() {} //add your goroutine function here. This should NOT stop until you recieve a "stop" command.
 
 	runtime.GOMAXPROCS(len(GlobalVars.ModuleRoutines))
