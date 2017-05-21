93069afc178efaa7782b2c6ea9c1624c699d6e4c
Shelvacu <shelvacu@gmail.com>
2017-05-20 18:02:07 -0700

    add simple keycode/mode handling, woo

--- src/github.com/Virepri/Shoraldele/input/whateveryouwannacallit.go
+++ src/github.com/Virepri/Shoraldele/input/whateveryouwannacallit.go
@@ -1,10 +1,27 @@
 package input
 
+//input manages modes
+
 import (
 	tm "github.com/jonvaldes/termo"
 	gv "github.com/Virepri/Shoraldele/GlobalVars"
 )
-	
+
+//http://stackoverflow.com/questions/14426366/what-is-an-idiomatic-way-of-representing-enums-in-go
+
+type ModeType uint8
+
+const (
+	Command ModeType = iota
+	Insert
+	Select
+)
+
+var CurrentMode ModeType //Defaults to 0, which is Command mode
+
+//Append to this array if you want to be notified when the mode changes
+var ModeChangeNotifiers [](chan<- ModeType)
+
 func Setup(_ string) {
 }
 
@@ -27,5 +44,18 @@ func Routine {
 	for {
 		keycode = <-keychan
 		
+	}
+}
+
+func changeMode (new_mode ModeType) {
+	if new_mode == CurrentMode {
+		// do nothing!
+		return
+	} else {
+		CurrentMode = new_mode
+		// TODO: This should probably dispatch threads so that someone adding a chan but not listening does not block
+		for _, chn = range ModeChangeNotifiers {
+			chn <- new_mode
+		}
 	}
 }
